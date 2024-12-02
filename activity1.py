02d440670ecdd862067fa27b142429b90654e16c
jyneeweenie <jynemarie1@gmail.com>
2024-12-02 12:19:50 +0300

    done

---
+++ activity1.py
@@ -0,0 +1,8 @@
+# Printing a greeting message
+print("hello , world!")
+# Printing my name
+print("My name is Jynemarie.")
+# Printing a fun fact about myself
+print("I love crotcheting.")
+# Perfoming and printing a simple calculation
+print("The result of 7 * 7 is: ", 7 * 7)
